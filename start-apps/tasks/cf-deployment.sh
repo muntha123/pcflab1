f593466ac3c893eae8a66c3e3a5109158ef55c51
kishorepkr <45389648+kishorepkr@users.noreply.github.com>
2019-03-15 11:13:55 +0000

    Update cf-deployment.sh

--- start-apps/tasks/cf-deployment.sh
+++ start-apps/tasks/cf-deployment.sh
@@ -21,9 +21,9 @@ echo "$CF_SUB_COMMAND the app"
 for app in $applications
 	do
 		eval "cf $CF_SUB_COMMAND $app"
-		status = $(echo $(cf app $app  | grep 'requested state:'| awk '{print $3}'))
+		status = $(cf app $app  | grep 'requested state:'| awk '{print $3}')
 	done
-	restart_fun () {
+	restart_fun (){
   		echo cf $CF_SUB_COMMAND $app
   		exit ()
 	}
