16c8fb2fe03b93cb84c1a415509c2380690410c5
Ubuntu <ubuntu@ip-10-0-0-160.ec2.internal>
2019-03-15 10:55:00 +0000

    restart script change

--- start-apps/tasks/cf-deployment.sh
+++ start-apps/tasks/cf-deployment.sh
@@ -14,11 +14,28 @@ echo "Target Org and Space"
 org_space="cf target -o $ORG -s $SPACE"
 eval $org_space
 
-applications=`cat pcflab/start-apps/app-list.json | jq -r '.[].application'`
+applications=`cat misc-repo/start-apps/app-list.json | jq -r '.[].application'`
 
+restart_fun ()
+{
+  echo cf $CF_SUB_COMMAND $app
+  exit ()
+}
 
 echo "$CF_SUB_COMMAND the app"
+
 for app in $applications
-do
-  eval "cf $CF_SUB_COMMAND $app"
-done
+	do
+		eval "cf $CF_SUB_COMMAND $app"
+		status = '(echo $(cf app $app  | grep 'requested state:'| awk '{print $3}'))'
+	done
+	count = 5
+	for((i=0;i<$count;i++))
+	{
+	if [ $status -eq 'started' ]
+		then 
+			echo "$app is started"
+		else
+			restart_fun
+	fi
+	}
